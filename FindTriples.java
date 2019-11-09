66d2dd5be6c637b434f0a31c38634b1b8aed1c23
gongzian <56396590+gongzian@users.noreply.github.com>
2019-11-09 18:50:13 -0500

    Add files via upload

--- FindTriples.java
+++ FindTriples.java
@@ -1,5 +1,5 @@
 /**
- *  Created by Zian Gong on 2019/11/8
+ *  Created by Zian Gong on 2019/11/9
  *
  *  In a list, find all possible three integers which sum equals 0
  *
@@ -10,46 +10,35 @@ public class FindTriples {
 
     public static void main(String[] args){
         FindTriples ft=new FindTriples();
-        int[] nums=new int[6];
-        nums[0]=-1;
-        nums[1]=0;
-        nums[2]=1;
-        nums[3]=2;
-        nums[4]=-1;
-        nums[5]=-4;
+        int[] nums = {-1,0,1,2,-1,-4,5,9,7,45,1,6,54,9,-9,-5,-4,-3};
         List<List<Integer>> res=ft.findTriples(nums);
         System.out.println(res);
     }
 
-    public List<List<Integer>> findTriples(int[] nums){
+    public List<List<Integer>> findTriples(int[] nums) {
         List<List<Integer>> res = new ArrayList();
-        for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]+nums[j]+nums[k]==0){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            int left = i+1;
+            int right = nums.length-1;
+            if (i == 0 || nums[i] != nums[i-1]) {
+                while (left < right) {
+                    if (nums[i] + nums[left] + nums[right] == 0) {
                         List<Integer> tt = new ArrayList();
                         tt.add(nums[i]);
-                        tt.add(nums[j]);
-                        tt.add(nums[k]);
-                        Collections.sort(tt);
-                        int eq = 0;//if eq=1 tt already in res; eq=0 add tt
-                        int ct=0;//to count how many elements are same in a pair of list
-                        for(int o=0;o<res.size();o++){
-                            ct=0;//refresh ct every pair of lists
-                            for(int p=0;p<tt.size();p++){
-                                if(tt.get(p)==res.get(o).get(p))
-                                    ct+=1;
-                            }
-                            if(ct==3){
-                                eq=1;
-                                break;
-                            }
-                        }
-                        if(eq!=1)
-                            res.add(tt);
-                        else
-                            eq = 0;
-                    }
+                        tt.add(nums[left]);
+                        tt.add(nums[right]);
+                        res.add(tt);
+                        while (nums[right] == nums[right-1])
+                            right--;
+                        while (nums[left] == nums[left+1])
+                            left++;
+                        right--;
+                        left++;
+                    } else if (nums[i] + nums[left] + nums[right] < 0)
+                        left++;
+                    else
+                        right--;
                 }
             }
         }
